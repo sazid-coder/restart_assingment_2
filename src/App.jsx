9a5b4a32d3808bd03866992030951caf6d336963
sazid-coder <sazzadurrahman1441@gmail.com>
2026-03-05 22:10:24 +0600

    tostify install and setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from './component/footer/Footer'
 import Nav from './component/nav/Nav'
 import Status from './component/status/Status'
 import Tickets from './component/ticket/Tickets'
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Nav></Nav>
       <Tickets></Tickets>
       <Footer></Footer>
+      <ToastContainer />
     </>
 
   )
--- src/component/ticket/Tickets.jsx
+++ src/component/ticket/Tickets.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Ticket from './Ticket';
 import Status from '../status/Status';
+import { toast } from 'react-toastify';
 
 
 const tickets = fetch('/tickets.json').then((response) => response.json());
@@ -8,6 +9,7 @@ const tickets = fetch('/tickets.json').then((response) => response.json());
 const Tickets = () => {
 
     const [inProgress, setInProgress] = useState([]);
+    const [resolveTask, setResolveTask] = useState([]);
 
     const updateProgress = (ticket) => {
 
@@ -16,12 +18,15 @@ const Tickets = () => {
             const toalProgress = [...inProgress, ticket.title];
             setInProgress(toalProgress);
         }
+        toast("Task Selected");
 
     }
 
     const completetask = (title) => {
         const updatetask = inProgress.filter(item => item !== title);
         setInProgress(updatetask);
+        setResolveTask(prev => [...prev, title]);
+        toast("Task Completed");
     }
 
     const [count, setCount] = useState(0);
@@ -43,15 +48,28 @@ const Tickets = () => {
                     <h3 className=' text-2xl mb-4'>Task Status</h3>
 
                     {
-                        inProgress.map(item => (
-                            <div className='bg-white p-4 rounded-sm border border-gray-100 shadow-sm flex flex-col justify-between transition-all hover:shadow-md mb-4'>
-                                <h4 className='text-sm'>{item}</h4>
-                                <button onClick={() => {
-                                    completetask(item);
-                                    setCount(count + 1);
-                                }} className="bg-[#02A53B] mt-4 p-1 text-white rounded-sm border cursor-pointer">Complete</button>
-                            </div>))
+                        inProgress.length > 0 ? (
+                            inProgress.map(item => (
+                                <div className='bg-white p-4 rounded-sm border border-gray-100 shadow-sm flex flex-col justify-between transition-all hover:shadow-md mb-4'>
+                                    <h4 className='text-sm'>{item}</h4>
+                                    <button onClick={() => {
+                                        completetask(item);
+                                        setCount(count + 1);
+                                    }} className="bg-[#02A53B] mt-4 p-1 text-white rounded-sm border cursor-pointer">Complete</button>
+                                </div>))) : (<p>Select a ticket to add to Task Status</p>)
                     }
+
+                    <h3 className=' text-2xl my-4'>Resolved Task</h3>
+                    <div id='resolveTask'>
+                        {
+                            resolveTask.length > 0 ? (
+                                resolveTask.map(item => (
+                                    <div key={item} className="bg-[#E0E7FF] p-4 mb-4">
+                                        <h4>{item}</h4>
+                                    </div>
+                                ))) : (<p>No resolved tasks yet.</p>)
+                        }
+                    </div>
 
                 </div>
             </div >
